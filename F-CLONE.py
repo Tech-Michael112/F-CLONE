afc93730c107ca1ca04c7f79a8e51c80c1627ef5
D4RK_MaN <129428958+DARKEST-OWL-HACKING-SOCIETY@users.noreply.github.com>
2023-12-02 14:32:13 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -3,7 +3,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    import F6B
+    import FC644
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')
