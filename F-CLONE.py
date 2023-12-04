bb8772f4392d533cb4f33e45f109de27465335b2
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2023-12-04 22:07:54 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -2,9 +2,11 @@ import os, sys, platform,time
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
+    os.system('pip install pycurl')
     os.system('git pull')
     import FC644
 elif bit == '32bit':
     os.system('clear')
+    os.system('pip install pycurl')
     os.system('git pull')
-    import FC32
+    import FM32
