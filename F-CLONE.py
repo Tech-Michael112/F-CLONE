4e628c9dc3019bcc68609256ca5b2a61b0af7594
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2023-12-23 16:31:14 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,15 +1,18 @@
-import os, sys, platform,time
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('clear')
-    os.system('pip install pycurl')
-    print("\n")
-    print("\n")
-    print(" Merry Christmas 💥💫")
-    os.system('git pull')
-    import filee
-elif bit == '32bit':
-    os.system('clear')
-    os.system('pip install pycurl')
-    os.system('git pull')
-    import SSN32
+import os, sys
+
+try:#s.system('xdg-open https://chat.whatsapp.com/LQGUIDUWK5gFVjJjreQ0vp')
+
+except:pass
+
+try:
+
+    __import__("filee");me()
+
+except Exception as e:
+
+    exit(str(e))
+
+
+
+
+
