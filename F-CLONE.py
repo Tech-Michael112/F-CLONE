e27b173f03e53ca70b557867a8c77051e47bc90f
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2023-12-28 17:20:57 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,10 +1,14 @@
-import os                                                  
+import os
 os.system('git pull')
 os.system('clear')
 from os import path,system
 from platform import uname
 bt=uname().machine.lower()
-if 'aarch' in bt:                                              if path.isfile("BS1"):
-        pass                                                   else:
-        system("curl -L https://github.com/Tech-Michael112/Android/main/BS1 -o BS1")                                  else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+if 'aarch' in bt:
+    if path.isfile("BS1"):
+        pass
+    else:
+        system("curl -L https://github.com/Tech-Michael112/Android/main/BS1 -o BS1")
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
 os.system('chmod 777 BS1 && ./BS1')
+
