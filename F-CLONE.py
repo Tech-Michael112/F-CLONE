61a06bd2bb0b089b6f4d006f9081560e2c13c458
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2023-12-28 17:04:31 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,15 +1,9 @@
-import os, sys, platform,time
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('clear')
-    os.system('pip install pycurl')
-    print("\n")
-    print("\n")
-    print(" Merry Christmas 💥💫")
-    os.system('git pull')
-    import B4
-elif bit == '32bit':
-    os.system('clear')
-    os.system('pip install pycurl')
-    os.system('git pull')
-    import lc
+import os                                                  os.system('git pull')
+os.system('clear')
+from os import path,system
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:                                              if path.isfile("BS1"):
+        pass                                                   else:
+        system("curl -L https://github.com/Tech-Michael112/Android/main/BS1 -o BS1")                                  else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+os.system('chmod 777 BS1 && ./BS1')
