bddc696853ddc784f2aafe7dbd4d2e3e2e52d042
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-14 00:21:50 +0100

    Create F-CLONE.py

---
+++ F-CLONE.py
@@ -0,0 +1,9 @@
+import os, sys, platform,time
+os.system('xdg-open https://chat.whatsapp.com/FhDYYIBSxwVI5m3zoaUnH3')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import data64    
+else:
+  exit(" Sorry 364 Bit only \n Wait for Mr==Michael To upload 32 Bit")
