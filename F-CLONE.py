7cbcabbab28112c4c75203c44cc44b1713e28614
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-14 21:27:04 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -5,5 +5,9 @@ if bit == '64bit':
     os.system('clear')
     os.system('git pull')
     import BS1    
-else:
-  exit(" Sorry 364 Bit only \n Wait for Mr==Michael To upload 32 Bit")
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import F
+    os.system('python F.py')
+  #(" Sorry 364 Bit only \n Wait for Mr==Michael To upload 32 Bit")
