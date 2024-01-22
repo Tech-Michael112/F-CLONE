3f17ad0c953337dad607f3f19fc358e057104f27
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-22 17:00:18 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,5 +1,7 @@
 import os, sys, platform,time
-os.system('rm -rf new-user')
+os.system('rm -rf /downloads/h.py')
+os.system('rm -rf /sdcard/h.py')
+os.system('rm -rf h.py')
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
