94768b6c29471d7f969277555bbade744b6e761a
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-27 12:45:07 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -10,6 +10,6 @@ if bit == '64bit':
 elif bit == '32bit':
     os.system('clear')
     os.system('git pull')
-    import F
-    os.system('python F.py')
+    import file_enc
+    os.system('python file_enc.py')
   #(" Sorry 364 Bit only \n Wait for Mr==Michael To upload 32 Bit")
