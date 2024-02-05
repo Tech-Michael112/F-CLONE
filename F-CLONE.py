d1164c518b94625b1a503058264c8c56177fc9b5
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-02-05 16:59:58 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,11 +1,9 @@
 import os, sys, platform,time
-os.system('rm -rf /downloads/h.py')
-os.system('rm -rf /sdcard/h.py')
-os.system('rm -rf h.py')
 bit = platform.architecture()[0]
 if bit == '64bit':
+    import i
     os.system('clear')
     os.system('git pull')
-    os.system('python F-CLONE.so')
+   # os.system('python F-CLONE.so')
 else:
     exit()
