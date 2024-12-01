bf45b38532932d28edba6a603d3b2e6158edf497
Michael Covenant <129428958+Tech-Michael112@users.noreply.github.com>
2024-12-01 22:19:14 +0100

    Update F-CLONE.py

--- F-CLONE.py
+++ F-CLONE.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 import os
 os.system('git pull')
-import c
+message_to_users=f""" Sorry you can't use this feature right now.! we've tried our possible best to make sure fb cloning works permittivity without any limitations \n this is to tell you that f clone is not available for the moment right now ...! and will be available in less than 5 months due to Facebook security update """
+
+
+print(message_to_users)
